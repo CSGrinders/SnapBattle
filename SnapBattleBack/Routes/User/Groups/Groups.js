e0aa517068075a8cc3ecec0b97146d1b15e079bd
PeterS-Kang <kang510@purdue.edu>
2024-02-15 14:12:17 -0500

    implemented controllers

--- SnapBattleBack/Routes/User/Groups/Groups.js
+++ SnapBattleBack/Routes/User/Groups/Groups.js
@@ -5,97 +5,15 @@ const User = require('../../../Models/User')
 
 const {
     inviteToGroup
-} = require("../../../Controllers/Groups/GroupInviteController")
+} = require("../../../Controllers/Groups/GroupInviteController");
 
-/**
-  * /groups/
-  * @params none
-  * @returns list of all groups
-  **/
+const { getGroups, createGroup, listUsers } = require("../../../Controllers/Groups/GroupActionsController");
 
-router.route('/').get((req, res) => {
-    Group.find()
-        .then(groups => res.json(groups))
-        .catch(err => res.status(400).json('Error: ' + err))
-})
+router.get('/', getGroups)
 
+router.post('/create', createGroup)
 
-/** 
-  *  /groups/create
-  *  @params username, groupName, maxUsers, 
-  *          timeStart, timeEnd, timeToVote,
-  *  
-  *  Creates a group
-  * 
-  *  @returns 200 for success, 404 for not found, 500 for server errs
-  **/
-
-router.route('/create').post(async(req, res) => {
-    try {
-        const username = req.body.username;
-        const groupName = req.body.groupName;
-        const maxUsers = req.body.maxUsers;
-        const timeStart = req.body.timeStart;
-        const timeEnd = req.body.timeEnd;
-        const timeToVote = req.body.timeToVote;
-        
-        const user = await User.findOne({username: username});
-        
-        if (user) {
-            const userList = []
-            userList.push(user._id)
-    
-            const prompts = []
-    
-            const newGroup = new Group({
-                name: groupName,
-                maxUsers: maxUsers,
-                timeStart: timeStart,
-                timeEnd: timeEnd,
-                userList: userList,
-                adminUserID: user._id,
-                timeToVote: timeToVote,
-                prompts: prompts
-            })
-    
-            await newGroup.save()
-    
-            user.groups.push(newGroup._id)
-            await user.save()
-    
-            res.status(200).json({
-                "group": newGroup,
-                "message": "New Group Created"
-            })
-        } else {
-            res.status(404).json("User not found")
-        }
-    } catch (error) {
-        res.status(500).json("Error: " + error)
-    }
-})
-
-/**
-  * /groups/users/:groupID
-  *  @params groupID
-  * 
-  *  @returns list of users
-  **/
-
-router.route('/users/:groupID').get(async(req, res) => {
-    try {
-        const groupID = req.params.groupID
-        const group = await Group.findById(groupID).populate('userList')
-
-        if (group) {
-            res.send(group.userList)
-        } else {
-            res.status(404).json("Group not found")
-        }
-    } catch (error) {
-        res.status(500).json("Error: " + error)
-    }
-})
+router.get('/list-users/:groupID', listUsers)
 
 router.post('/invite', inviteToGroup)
 
