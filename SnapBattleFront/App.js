685a3763812c86958b3c9cb2c7c4ea3e097cd07d
Alejandro S <alejandrosanudo10@gmail.com>
2024-02-15 14:45:10 -0500

    Added fonts

--- SnapBattleFront/App.js
+++ SnapBattleFront/App.js
@@ -1,20 +1,30 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
+import OpenSansRegular from './assets/fonts/OpenSansRegular.ttf';
+import OpenSansBold from './assets/fonts/OpenSansExtraBold.ttf';
 const Stack = createNativeStackNavigator()
-
 import Testing from "./Pages/Testing"
 import {ThemeProvider} from "@rneui/themed";
 import {theme} from "./Theme/Theme.js"
-import GroupMembers from "./Pages/Group/GroupMembers";
+import {useFonts} from "expo-font";
 
 
 export default function App() {
+
+    const [fontsLoaded, fontError] = useFonts({ //Load fonts
+        'OpenSansRegular': OpenSansRegular,
+        'OpenSansBold': OpenSansBold,
+    });
+
+    if (!fontsLoaded && !fontError) {
+        return null;
+    }
+
     return (
         <ThemeProvider theme={theme}>
             <NavigationContainer>
                 <Stack.Navigator screenOptions={{headerShown: false}}>
-                    <Stack.Screen name="GroupMembers" component={GroupMembers}/>
+                    <Stack.Screen name="Testing" component={Testing}/>
                 </Stack.Navigator>
             </NavigationContainer>
         </ThemeProvider>
